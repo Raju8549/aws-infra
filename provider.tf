e18e2b255ecba5df5df09836295f0c5116248c93
Raju8549 <152939408+Raju8549@users.noreply.github.com>
2024-05-15 14:31:41 +0530

    provider updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
+# AWS Provider Info
 provider "aws" {
   region     = "us-east-1"
-  access_key = "var.aws_acc_key"
-  secret_key = "var.aws_sec_key"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
